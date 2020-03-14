64a7c9f76e070222a641c8331b829e7637cab79b
yowmacs <58980027+yowmacs@users.noreply.github.com>
2020-03-14 10:59:11 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'crud');
+define('DB_SERVER', 'macasactivity3.mysql.database.azure.com');
+define('DB_USERNAME', 'yowmacs@macasactivity3');
+define('DB_PASSWORD', 'Rayukapeach16');
+define('DB_NAME', 'activity3azure');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
