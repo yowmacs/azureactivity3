edcdb87b50436c8d1d8176b109e57e600f94762b
yowmacs <58980027+yowmacs@users.noreply.github.com>
2020-03-14 09:56:05 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php
+$servername = "macasactivity3.mysql.database.azure.com";
+$username = "yowmacs@macasactivity3";
+$password = "Rayukapeach16";
+$dbname = "activityazure";
 
-	echo "Hellow World!";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
+$sql = "SELECT id, description, count FROM inventory";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Product: " . $row["description"]. " - Count " . $row["count"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
 ?>
